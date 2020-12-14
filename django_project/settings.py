46a5865d584c5156ecadfa07530138f3003b44ec
lpilas24 <leiva20152@gmail.com>
2020-12-14 18:31:54 -0500

    variables de env de heroku agregadas

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zrg$qpk=f2qryo@a29q_m1ubc)y^w&alxvjq-x(&w5e%blqqx='
-
+#SECRET_KEY = 'zrg$qpk=f2qryo@a29q_m1ubc)y^w&alxvjq-x(&w5e%blqqx='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
